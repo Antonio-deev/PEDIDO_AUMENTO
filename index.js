c48dbf127b4602d6bcfee10022e5bf5a60da05cb
Antonio-deev <antonhomarcos1309@gmail.com>
2025-12-31 16:18:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const contadorSpan = document.getElementById("vezes");
 let vezesNegou = 0;
 
 function fugirBotao() {
-    // largura e altura máximas para que o botão não saia da tela
+    // largura e altura máximas para manter dentro da tela
     const maxX = window.innerWidth - botaoNao.offsetWidth;
     const maxY = window.innerHeight - botaoNao.offsetHeight;
 
-    // posições aleatórias dentro da tela
     const x = Math.random() * maxX;
     const y = Math.random() * maxY;
 
@@ -23,3 +22,9 @@ botaoNao.addEventListener("mouseenter", fugirBotao);
 
 // Celular: toque
 botaoNao.addEventListener("touchstart", fugirBotao);
+
+// Botão "Sim" navega
+const botaoSim = document.getElementById("sim");
+botaoSim.addEventListener("click", () => {
+    window.location.href = "indexII.html";
+});
